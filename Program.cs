991b86ef51715e3a8673582ce1b70558a41bd88f
Silambarasan Ilango <silambarasani@syncfusion.com>
2020-03-27 17:20:29 +0530

    Blazor client-side app committed.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.Blazor;
+
+namespace BlazorClientApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSyncfusionBlazor();
+            await builder.Build().RunAsync();
+        }
+    }
+}
